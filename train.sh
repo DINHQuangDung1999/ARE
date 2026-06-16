60f4b1a32879e373846ea4c9d230d060361211fa
DINHQuangDung1999 <dqdung999@gmail.com>
2026-06-16 20:44:54 +0200

    Commit

---
+++ train.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+cd "${ROOT_DIR}"
+export PYTHONPATH="${ROOT_DIR}/rewarped:${PYTHONPATH:-}"
+
+for seed in {1,}
+do
+  python -m mineral.scripts.run \
+    task=Rewarped \
+    agent=DFlexSAPOGAE \
+    task.env.env_suite=dflex\
+    task.env.env_name=Go2 \
+    task.env.numEnvs=64\
+    logdir=workdir/Go2-SAPOGAEAdpt/seed${seed} \
+    agent.shac.max_epochs=2500 \
+    agent.shac.max_agent_steps=1e7 \
+    agent.shac.horizon_len=32\
+    agent.shac.actor_loss_type=adpt-gae\
+    agent.shac.num_lambda_batches=4\
+    agent.network.actor_kwargs.mlp_kwargs.units=\[256,128\] \
+    agent.network.critic_kwargs.mlp_kwargs.units=\[256,128\] \
+    run=train_eval seed=${seed} \
+    wandb.name=Go2-SAPOGAEAdpt-${seed} \
+    wandb.mode=online \
+    wandb.project=Test
+done
